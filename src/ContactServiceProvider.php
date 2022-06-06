a6f19f8343e1a89bce3a9852fd8fa1d06130d72f
Hein Thu Ya <heinthuya12@gmail.com>
2022-06-06 15:20:03 +0630

    Fix ContactServiceProvider

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -11,9 +11,6 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views', 'contact');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->mergeConfigFrom(
-            __DIR__.'/config/contact.php', 'contact'
-        );
 
         $this->publishes([
             __DIR__.'/config/contact.php' => config_path('contact.php'),
@@ -23,6 +20,8 @@ class ContactServiceProvider extends ServiceProvider
 
     public function register()
     {
-
+        $this->mergeConfigFrom(
+            __DIR__.'/config/contact.php', 'contact'
+        );
     }
 }
